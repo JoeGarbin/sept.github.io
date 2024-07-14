ba06f4b8c906fc092fa98a0636dbce0d864d6176
Frank Joe <qiaojiabin777@gmail.com>
2024-07-14 16:38:54 +0800

    add deploy script

---
+++ deploy2Github.sh
@@ -0,0 +1,23 @@
+# 部署到github pages
+
+# 打包
+pnpm run build
+
+# 切换分支
+git checkout -b githubPages
+
+# 删除旧文件
+rm -rf ./sept/*
+
+# 复制新文件
+cp -r dist/* ./sept
+
+# 提交
+git add .
+git commit -m "deploy"
+
+# 推送
+git push origin githubPages
+
+# 切换回主分支
+git checkout -
--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  base: '/sept/',
+  build: {
+    outDir: 'dist'
+  },
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
